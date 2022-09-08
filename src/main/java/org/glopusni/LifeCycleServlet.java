3058b21dde6e6ecad324b03c6d7eeaba50e393ac
glopusni <gabesz.lopusni@gmail.com>
2022-09-08 18:19:06 +0200

    initial version

---
+++ src/main/java/org/glopusni/LifeCycleServlet.java
@@ -0,0 +1,36 @@
+package org.glopusni;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class LifeCycleServlet extends HttpServlet {
+
+    public LifeCycleServlet() {
+        System.out.println("LifeCycleServlet has been created.");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Destroy method in LifeCycleServlet called");
+    }
+
+    @Override
+    public void init() throws ServletException {
+
+        System.out.println("Init method in LifeCycleServlet called");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet method in LifeCycleServlet called");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost method in LifeCycleServlet called");
+    }
+}
