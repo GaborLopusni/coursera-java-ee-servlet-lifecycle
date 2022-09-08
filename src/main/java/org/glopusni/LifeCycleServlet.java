f107c56cef08e7c908a84b1e47e416a2f0d28c63
glopusni <gabesz.lopusni@gmail.com>
2022-09-08 18:54:25 +0200

    fix servlet

--- src/main/java/org/glopusni/LifeCycleServlet.java
+++ src/main/java/org/glopusni/LifeCycleServlet.java
@@ -1,12 +1,14 @@
 package org.glopusni;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
+@WebServlet(urlPatterns = "/LifecycleServlet")
 public class LifeCycleServlet extends HttpServlet {
 
     public LifeCycleServlet() {
@@ -20,13 +22,13 @@ public class LifeCycleServlet extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-
         System.out.println("Init method in LifeCycleServlet called");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("doGet method in LifeCycleServlet called");
+        req.getSession().invalidate();
     }
 
     @Override
